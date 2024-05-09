d23fa055fe6c4257585e9cdb5357c09806215b79
SG Studio Club <52781798+sgstudioclub@users.noreply.github.com>
2024-05-09 21:43:19 +0700

    Update mycode.py

--- mycode.py
+++ mycode.py
@@ -1,5 +1,7 @@
 def sum_of_multiples(n: int, x: int) -> int:
-    sum = x * n
+    sum = 0
+    for i in range(x, n+1, x):
+        sum += i
     return sum
 
 def min_window_substring(s: str, t: str) -> str:
